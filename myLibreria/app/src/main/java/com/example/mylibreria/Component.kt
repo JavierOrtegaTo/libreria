c9c031d9947410cd734833f8cb0e647b7af6636b
JavierOrtegaTo <143211403+JavierOrtegaTo@users.noreply.github.com>
2024-07-10 17:33:36 -0400

    Add files via upload

---
+++ myLibreria/app/src/main/java/com/example/mylibreria/Component.kt
@@ -0,0 +1,47 @@
+package com.example.mylibreria
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.widget.Button
+
+class Component {
+
+    lateinit var dialog: Dialog
+
+    fun getMensajeAlertaReserva(
+        context: Context,
+        onClickListener : () -> Unit
+    ): Dialog {
+        dialog = Dialog(context, R.style.wideDialog)
+
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val viewDialog = inflater.inflate(R.layout.modal_custom, null)
+
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setContentView(viewDialog)
+        dialog.setCancelable(false)
+
+        val btnCancelar: Button = viewDialog.findViewById(R.id.btnCancelar)
+        val btnVerMisReservas: Button = viewDialog.findViewById(R.id.btnVerMisReservas)
+
+
+        btnCancelar.let {
+            it.setOnClickListener {
+                dialog.dismiss()
+            }
+        }
+
+        btnVerMisReservas.let {
+            it.setOnClickListener {
+                dialog.dismiss()
+                onClickListener.invoke()
+            }
+        }
+
+        return dialog
+    }
+
+}
